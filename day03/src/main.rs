80ebf2ca4954e83a41c6aa7c388ccfe282fee462
BeatButton <thebeatbutton@gmail.com>
2020-12-02 23:06:10 -0700

    clean up day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,23 +1,21 @@
-const INPUT: &str = include_str!("input");
+const INPUT: [[u8; 31]; 323] = include!("input");
+const WIDTH: usize = INPUT[0].len();
 const OVER: [usize; 5] = [1, 3, 5, 7, 1];
 const DOWN: [usize; 5] = [1, 1, 1, 1, 2];
 
 fn main() {
-    let input: Vec<&str> = INPUT.lines().collect();
-    let width = input[0].len();
-    let mut out: u64 = 1;
-    for (over, down) in OVER.iter().zip(&DOWN) {
-        let mut y = 0;
-        let mut x = 0;
-        let mut num_trees = 0;
-        while y < input.len() {
-            if input[y].chars().nth(x % width).unwrap() == '#' {
-                num_trees += 1;
-            }
-            y += down;
-            x += over;
-        }
-        out *= num_trees;
-    }
-    println!("{}", out);
+    println!(
+        "{}",
+        OVER.iter()
+            .zip(&DOWN)
+            .map(|(&over, &down)| {
+                INPUT
+                    .iter()
+                    .step_by(down)
+                    .zip((0_usize..).step_by(over))
+                    .filter(|(line, idx)| line[idx % WIDTH] == b'#')
+                    .count() as u64
+            })
+            .product::<u64>()
+    );
 }
