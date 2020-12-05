1c2eca8d2e658c59c1d73c4e51e8df67b9473b05
BeatButton <thebeatbutton@gmail.com>
2020-12-04 23:22:34 -0700

    with the power of itertools...

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,28 +1,26 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("input");
 
 fn main() {
-    let mut ids: Vec<u16> = INPUT
-        .lines()
-        .map(|pass| {
-            u16::from_str_radix(
-                pass.replace(['B', 'R'].as_ref(), "1")
-                    .replace(['F', 'L'].as_ref(), "0")
-                    .as_str(),
-                2,
-            )
-            .unwrap()
-        })
-        .collect();
-
-    ids.sort_unstable();
-
     println!(
         "{}",
-        ids.iter()
-            .map(|&i| i + 1)
-            .zip(ids.iter().skip(1))
-            .find(|&(left, &right)| left != right)
+        INPUT
+            .lines()
+            .map(|pass| {
+                u16::from_str_radix(
+                    pass.replace(['B', 'R'].as_ref(), "1")
+                        .replace(['F', 'L'].as_ref(), "0")
+                        .as_str(),
+                    2,
+                )
+                .unwrap()
+            })
+            .sorted()
+            .tuple_windows()
+            .map(|(a, b)| (a + 1, b))
+            .find(|(a, b)| a != b)
             .unwrap()
             .0
-    )
+    );
 }
