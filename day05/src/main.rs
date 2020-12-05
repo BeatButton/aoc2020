c92146a0ce9d474e97c6d055488ebd8d5c665744
BeatButton <thebeatbutton@gmail.com>
2020-12-04 23:14:38 -0700

    Be less bad

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,46 +1,28 @@
-use std::collections::HashSet;
-
 const INPUT: &str = include_str!("input");
 
 fn main() {
-    let mut set: HashSet<u16> = (0..=1023).collect();
-    for pass in INPUT.lines() {
-        let mut iter = pass.chars();
-        let row = u16::from_str_radix(
-            iter.by_ref()
-                .take(7)
-                .map(|ch| match ch {
-                    'B' => '1',
-                    'F' => '0',
-                    _ => panic!(),
-                })
-                .collect::<String>()
-                .as_str(),
-            2,
-        )
-        .unwrap();
-        let col = u16::from_str_radix(
-            iter.take(3)
-                .map(|ch| match ch {
-                    'R' => '1',
-                    'L' => '0',
-                    _ => '_',
-                })
-                .collect::<String>()
-                .as_str(),
-            2,
-        )
-        .unwrap();
-        let id = row * 8 + col;
-        set.remove(&id);
-    }
-    let mut i = 0;
-    while set.remove(&i) {
-        i += 1;
-    }
-    i = 1023;
-    while set.remove(&i) {
-        i -= 1;
-    }
-    println!("{}", set.iter().next().unwrap());
+    let mut ids: Vec<u16> = INPUT
+        .lines()
+        .map(|pass| {
+            u16::from_str_radix(
+                pass.replace(['B', 'R'].as_ref(), "1")
+                    .replace(['F', 'L'].as_ref(), "0")
+                    .as_str(),
+                2,
+            )
+            .unwrap()
+        })
+        .collect();
+
+    ids.sort_unstable();
+
+    println!(
+        "{}",
+        ids.iter()
+            .map(|&i| i + 1)
+            .zip(ids.iter().skip(1))
+            .find(|&(left, &right)| left != right)
+            .unwrap()
+            .0
+    )
 }
