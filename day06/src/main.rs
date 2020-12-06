3c8cc8b389f7eba088fb9fb666455abd44a73ba2
BeatButton <thebeatbutton@gmail.com>
2020-12-06 16:12:50 -0700

    map map map

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,17 +3,21 @@ use std::collections::HashSet;
 const INPUT: &str = include_str!("input");
 
 fn main() {
-    let mut total = 0;
-    for group in INPUT.split("\n\n") {
-        let mut group = group.lines();
-        let mut set: HashSet<char> = group.next().unwrap().chars().collect();
-        for person in group {
-            set = set
-                .intersection(&person.chars().collect::<HashSet<char>>())
-                .cloned()
-                .collect()
-        }
-        total += set.len()
-    }
-    println!("{}", total);
+    println!(
+        "{}",
+        INPUT
+            .split("\n\n")
+            .map(|group| group.lines())
+            .map(|mut group| (group.next().unwrap().chars().collect::<HashSet<_>>(), group))
+            .map(|(first, group)| {
+                group
+                    .fold(first, |set, person| {
+                        set.intersection(&person.chars().collect::<HashSet<char>>())
+                            .cloned()
+                            .collect()
+                    })
+                    .len()
+            })
+            .sum::<usize>()
+    );
 }
