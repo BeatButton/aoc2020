1c0c4521362c3c30d53eccf79cd4e47c61f25a1f
BeatButton <thebeatbutton@gmail.com>
2020-12-06 22:47:18 -0700

    d7p1

---
+++ day07/src/main.rs
@@ -0,0 +1,34 @@
+use petgraph::graphmap::DiGraphMap;
+
+const INPUT: &str = include_str!("input");
+
+fn main() {
+    let mut graph: DiGraphMap<&str, usize> = DiGraphMap::new();
+    for line in INPUT.lines() {
+        let mut split = line[..line.len() - 1].split(" contain ");
+        let key = split.next().unwrap().trim_end_matches('s');
+        let values = split.next().unwrap();
+        graph.add_node(key);
+        if values == "no other bags" {
+            continue;
+        }
+        for value in values.split(", ") {
+            let amount: usize = value[0..1].parse().unwrap();
+            let value = &value[2..].trim_end_matches('s');
+
+            graph.add_node(value);
+            graph.add_edge(key, value, amount);
+        }
+    }
+
+    let mut total = 0;
+
+    for node in graph.nodes() {
+        if petgraph::algo::has_path_connecting(&graph, node, "shiny gold bag", None) {
+            total += 1;
+        }
+    }
+    total -= 1;
+
+    println!("{}", total);
+}
