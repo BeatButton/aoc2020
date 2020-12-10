bf111c40d8b5728df1ef4478faef4bbd3af0ff9f
BeatButton <thebeatbutton@gmail.com>
2020-12-09 23:20:42 -0700

    d10p2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,43 +1,38 @@
-use itertools::Itertools;
+use lru::LruCache;
 
 const INPUT: &str = include_str!("input");
 
-fn fuck(adapters: &[u16], path: Vec<u16>) -> Option<Vec<u16>> {
+fn fuck<'a>(adapters: &'a [u16], path: Vec<u16>, cache: &mut LruCache<&'a [u16], usize>) -> usize {
+    if let Some(n) = cache.get(&adapters) {
+        return *n;
+    }
     if adapters.is_empty() {
-        return Some(path);
+        1
+    } else {
+        let prev = *path.last().unwrap();
+        adapters
+            .iter()
+            .enumerate()
+            .take_while(|(_, v)| (prev..=(prev + 3)).contains(v))
+            .map(|(idx, &adapter)| {
+                let mut path = path.clone();
+                path.push(adapter);
+                let adapts = &adapters[idx + 1..];
+                let out = fuck(adapts, path, cache);
+                cache.put(adapts, out);
+                out
+            })
+            .sum()
     }
-    let prev = *path.last().unwrap();
-    let limit = prev + 3;
-    for (idx, &adapter) in adapters
-        .iter()
-        .enumerate()
-        .take_while(|(_, v)| (prev..=limit).contains(v))
-    {
-        let mut path = path.clone();
-        path.push(adapter);
-        let left = &adapters[0..idx];
-        let right = &adapters[idx + 1..];
-        let adapters: Vec<u16> = left.iter().chain(right).cloned().collect();
-        if let Some(out) = fuck(&adapters, path) {
-            return Some(out);
-        }
-    }
-    None
 }
 
 fn main() {
     let mut input: Vec<u16> = INPUT.lines().map(|line| line.parse().unwrap()).collect();
     input.sort_unstable();
 
-    let path = fuck(&input, vec![0]).unwrap();
+    let mut cache = LruCache::new(1_000_000_000);
 
-    let (one, three) =
-        path.iter()
-            .tuple_windows()
-            .fold((0, 0), |(one, three), (from, to)| match to - from {
-                1 => (one + 1, three),
-                3 => (one, three + 1),
-                _ => (one, three),
-            });
-    println!("{}", one * (three + 1));
+    let out = fuck(&input, vec![0], &mut cache);
+
+    println!("{}", out);
 }
