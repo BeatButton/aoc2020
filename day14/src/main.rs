ee0ddc83e8933774f9e129a3fd298126263155d5
BeatButton <thebeatbutton@gmail.com>
2020-12-13 23:45:27 -0700

    d14p2
    forgot to commit it when I finished it oops

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,40 +1,55 @@
+use itertools::Itertools;
+
+use fnv::FnvHashMap as HashMap;
+
 const INPUT: &str = include_str!("input");
 
 fn main() {
     let mut one_mask = 0_u64;
-    let mut zero_mask = 2_u64.pow(35) - 1;
-    let mut mem = vec![0_u64; 100_000];
+    let mut floating_masks: Vec<u64> = vec![];
+    let mut mem: HashMap<u64, u64> = HashMap::default();
     for line in INPUT.lines() {
         let mut iter = line.split(" = ");
         let instr = iter.next().unwrap();
         let value = iter.next().unwrap();
         if instr == "mask" {
-            zero_mask = value
-                .chars()
-                .rev()
-                .enumerate()
-                .map(|(idx, ch)| if ch == '0' { 0 } else { 2_u64.pow(idx as u32) })
-                .sum();
-
             one_mask = value
                 .chars()
                 .rev()
                 .enumerate()
                 .map(|(idx, ch)| if ch == '1' { 2_u64.pow(idx as u32) } else { 0 })
                 .sum();
+            floating_masks.clear();
+            floating_masks.extend(value.chars().rev().enumerate().filter_map(|(idx, ch)| {
+                if ch == 'X' {
+                    Some(2_u64.pow(idx as u32))
+                } else {
+                    None
+                }
+            }))
         } else {
-            let addr = instr
+            let mut addr = instr
                 .chars()
                 .skip(4)
                 .take_while(|&ch| ch != ']')
                 .collect::<String>()
-                .parse::<usize>()
+                .parse::<u64>()
                 .unwrap();
-            let mut value = value.parse::<u64>().unwrap();
-            value &= zero_mask;
-            value |= one_mask;
-            mem[addr] = value;
+            let value = value.parse::<u64>().unwrap();
+            addr |= one_mask;
+
+            for &mask in &floating_masks {
+                addr &= !mask;
+            }
+
+            for num_masks in 0..=floating_masks.len() {
+                for masks in floating_masks.iter().cloned().combinations(num_masks) {
+                    let a = masks.iter().fold(addr, |acc, &mask| acc | mask);
+
+                    mem.insert(a, value);
+                }
+            }
         }
     }
-    println!("{}", mem.iter().sum::<u64>());
+    println!("{}", mem.values().sum::<u64>());
 }
