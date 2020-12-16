85c8f159b105f23fc5967ee5c1624dee1c2ec240
BeatButton <thebeatbutton@gmail.com>
2020-12-15 23:37:18 -0700

    d16p2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -16,8 +16,8 @@ fn main() {
                 .split(" or ")
                 .map(|range| {
                     let mut range = range.split('-');
-                    let start: u16 = range.next().unwrap().parse().unwrap();
-                    let stop: u16 = range.next().unwrap().parse().unwrap();
+                    let start: u64 = range.next().unwrap().parse().unwrap();
+                    let stop: u64 = range.next().unwrap().parse().unwrap();
                     start..=stop
                 })
                 .collect::<Vec<_>>();
@@ -25,33 +25,79 @@ fn main() {
         })
         .collect::<HashMap<_, _>>();
 
-    let _field_names = fields.keys().collect::<HashSet<_>>();
+    let field_names = fields.keys().collect::<HashSet<_>>();
 
-    let _my_ticket = iter
+    let my_ticket = iter
         .by_ref()
         .nth(1)
         .unwrap()
         .split(',')
-        .map(str::parse::<u16>)
+        .map(str::parse::<u64>)
         .map(Result::unwrap)
         .collect::<Vec<_>>();
 
-    let mut out = 0;
+    let mut possible_fields = vec![field_names; my_ticket.len()];
 
     for ticket in iter
         .skip(2)
-        .map(|line| line.split(',').map(str::parse::<u16>).map(Result::unwrap))
+        .map(|line| {
+            line.split(',')
+                .map(str::parse::<u64>)
+                .map(Result::unwrap)
+                .collect::<Vec<_>>()
+        })
+        .filter(|ticket| {
+            ticket
+                .iter()
+                .all(|value| fields.values().flatten().any(|range| range.contains(value)))
+        })
     {
-        for value in ticket {
-            if !fields
-                .values()
-                .flatten()
-                .any(|range| range.contains(&value))
-            {
-                out += value;
+        for (value, possible_fields) in ticket.into_iter().zip(possible_fields.iter_mut()) {
+            for field in possible_fields.clone() {
+                if !fields
+                    .get(field)
+                    .unwrap()
+                    .iter()
+                    .any(|range| range.contains(&value))
+                {
+                    possible_fields.remove(field);
+                }
             }
         }
     }
 
-    println!("{}", out)
+    loop {
+        let mut changed = false;
+        for fields in possible_fields.clone() {
+            if fields.len() == 1 {
+                let field = fields.iter().next().unwrap();
+                for possibilities in possible_fields.iter_mut() {
+                    if possibilities.len() == 1 {
+                        continue;
+                    }
+                    let removed = possibilities.remove(field);
+                    changed |= removed;
+                }
+            }
+        }
+
+        if !changed {
+            break;
+        }
+    }
+
+    let out = my_ticket
+        .into_iter()
+        .zip(possible_fields)
+        .map(|(value, fields)| {
+            println!("{:?}", fields);
+            if fields.into_iter().next().unwrap().starts_with("departure") {
+                value
+            } else {
+                1
+            }
+        })
+        .product::<u64>();
+
+    println!("{}", out);
 }
